9c9a4ab6ea2a6bed8016b40db2821e6321b02c6c
StefanPfeiffer80 <38919382+StefanPfeiffer80@users.noreply.github.com>
2019-07-30 12:58:01 +0200

    Update FASP_unoise.sh
    vsearch issue fixed

--- Scripts/FASP_unoise.sh
+++ Scripts/FASP_unoise.sh
@@ -33,9 +33,7 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "Sequence length trimming successfull; Output file: otus_sorted_UN.fasta"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 #4. Make (Z)OTU table
-vsearch -otutab raw.fq -usearch_global filteredstripped.fa --db otus_sorted_UN.fasta --id 0.99 --otutabout otutab_UN_raw.txt
-if [ $? -ne 0 ]; then
-  ./US_10.240 -otutab raw.fq -otus otus_sorted_UN.fasta -otutabout otutab_UN_raw.txt
+./US_10.240 -otutab raw.fq -otus otus_sorted_UN.fasta -otutabout otutab_UN_raw.txt
 fi
 #5. Uncross(Z)OTU table to remove spurious (Z)OTUs generated via cross-talk 
 ./US_10.240 -uncross otutab_UN_raw.txt -tabbedout outUN.txt -report repUN.txt -otutabout otutab_UN_uncrossed.txt
